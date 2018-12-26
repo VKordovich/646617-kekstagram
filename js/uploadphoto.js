12926945b9733cee080e5f507258134830f80089
VKordovich <VKordovich@gmail.com>
2018-12-26 13:39:28 +0500

    Lint

--- js/uploadphoto.js
+++ js/uploadphoto.js
@@ -1,24 +1,20 @@
 'use strict';
 (function () {
-    var fileChooser = document.querySelector('.img-upload__input');
-    var preview = document.querySelector('.img-upload__preview').querySelector('img');
-    var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
-    fileChooser.addEventListener('change', function () {
-        var file = fileChooser.files[0];
-        var fileName = file.name.toLowerCase();
-
-        var matches = FILE_TYPES.some(function (it) {
-            return fileName.endsWith(it);
-        });
-
-        if (matches) {
-            var reader = new FileReader();
-
-            reader.addEventListener('load', function () {
-                preview.src = reader.result;
-            });
-
-            reader.readAsDataURL(file);
-        }
+  var fileChooser = document.querySelector('.img-upload__input');
+  var preview = document.querySelector('.img-upload__preview').querySelector('img');
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+  fileChooser.addEventListener('change', function () {
+    var file = fileChooser.files[0];
+    var fileName = file.name.toLowerCase();
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
     });
+    if (matches) {
+      var reader = new FileReader();
+      reader.addEventListener('load', function () {
+        preview.src = reader.result;
+      });
+      reader.readAsDataURL(file);
+    }
+  });
 })();
